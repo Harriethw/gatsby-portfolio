e32552ebce48ae67fcaeb617d0f855c1020da383
Harriet Horobin-Worley <Harriet.HorobinWorley@aexp.com>
2019-02-22 15:19:18 +0000

    edited contact section

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,8 +9,8 @@ class Header extends React.Component {
             <header id="header">
                 <div className="inner">
                     <a href="#" className="image avatar"><img src={avatar} alt="" /></a>
-                    <h1><strong>I'm Harriet Horobin-Worley</strong>
-                      <br />a creative software engineer, <br />editor and creator<br />
+                  <h1><strong>Harriet Horobin-Worley</strong>
+                    <br/>Creative software engineer, <br/>editor and creator<br/>
                     </h1>
                 </div>
                 <Footer />
